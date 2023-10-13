3300f66378c45a348b69b8e55dff1237f45c1193
natyfbg <98698451+natyfbg@users.noreply.github.com>
2023-10-12 17:56:56 -0700

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import React from "react";
+import {useEffect, useState} from "react";
+import MovieCard from "./MovieCard";
+import './App.css'
+import searchIcon from './search.svg'
 
-//d14b9b8a
 
+const API_KEY = "d14b9b8a";
+const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
 
+const movie1 = {
+    "Title": "Harry Potter and the Half-Blood Prince",
+    "Year": "2009",
+    "imdbID": "tt0417741",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_SX300.jpg"
+}
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('harry potter')
+    }, []);
     return(
-        <h1>Apps</h1>
+    <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+            <input
+            placeholder="Search for movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
+
+            <img
+            src={searchIcon}
+            alt="search"
+            onClick={() => searchMovies(searchTerm)}
+            />
+        </div>
+
+        {movies ?.length > 0
+            ?(
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+                    ))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No Movies found</h2>
+                </div>
+            )}
+    </div>
     );
 }
 
